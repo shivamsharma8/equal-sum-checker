c9c4a8ee3b288ac7c824f53c78d598a1c7c56f4e
shivamsharma8 <113785642+shivamsharma8@users.noreply.github.com>
2022-09-30 21:37:32 +0530

    Add files via upload

---
+++ EqualSumChecker.java
@@ -0,0 +1,15 @@
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum (int a , int b , int c) {
+
+        if (a+b == c) {
+            return true;
+
+        }
+        else {
+
+            return false;
+
+        }
+    }
+}
